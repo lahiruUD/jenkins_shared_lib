05479cb5269327d40c59fa59a0a5a4ded7c8533c
lahiruUD <l.udayanga@yahoo.com>
2024-02-25 07:18:23 +0530

    added-2

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,6 +1,8 @@
-def call(credentialsId) {
-
-   withSonarQubeEnv(credentialsId: credentialsId) {
-        sh 'mvn clean package sonar:sonar'
+// Define a function called sonarQubeAnalysis
+def sonarQubeAnalysis() {
+    // Use withSonarQubeEnv to set up SonarQube environment
+    withSonarQubeEnv('sonar') {
+        // Execute the SonarQube scanner command
+        sh "$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectKey=Full-Stack -Dsonar.projectName=Full-Stack -Dsonar.java.binaries=."
     }
 }
