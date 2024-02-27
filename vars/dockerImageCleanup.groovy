dda48f9f2ee5f7ed12019e416625d6580bcd987f
lahiruUD <l.udayanga@yahoo.com>
2024-02-27 08:25:45 +0530

    added-4

---
+++ vars/dockerImageCleanup.groovy
@@ -0,0 +1,8 @@
+def call(String project, String ImageTag, String hubUser) {
+
+    sh """ 
+        docker rmi tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+        docker rmi tag ${hubUser}/${project} ${hubUser}/${project}:latest
+    """
+
+}
