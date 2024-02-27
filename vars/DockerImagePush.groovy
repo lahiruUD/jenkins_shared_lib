e6c449dd49d8baeb1636b1b95cb57bab47b24439
lahiruUD <l.udayanga@yahoo.com>
2024-02-27 07:44:31 +0530

    added-2

--- vars/DockerImagePush.groovy
+++ vars/DockerImagePush.groovy
@@ -1,10 +1,12 @@
 def call(String project, String ImageTag, String hubUser) {
-
-    withDockerRegistry(credentialsId: 'docker-cred-new') {
-        
-     sh "docker image push ${hubUser}/${project}:${ImageTag}"
-     sh "docker image push ${hubUser}/${project}:latest"
-
-}
+    withCredentials([usernamePassword(
+            credentialsId: "docker-cred",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u '$USER' -p '$PASS'"
+        }
+    sh "docker image push ${hubUser}/${project}:${ImageTag}"
+    sh "docker image push ${hubUser}/${project}:latest"
 
 }
