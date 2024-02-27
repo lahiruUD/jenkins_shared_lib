52af18181da68aa299b0ded2a63e00191a448d60
lahiruUD <l.udayanga@yahoo.com>
2024-02-27 11:46:20 +0530

    added-4

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -16,6 +16,6 @@ def call(String project, String ImageTag, String hubUser) {
 def call(String aws_account_id, String region, String ecr_repoName) {
     sh """ 
         aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-        docker ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+        docker push ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
