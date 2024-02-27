b8a09ce10f3b56c1ab20b5e150880240912d9d34
lahiruUD <l.udayanga@yahoo.com>
2024-02-27 11:41:46 +0530

    added-4

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,3 +1,4 @@
+/*
 def call(String project, String ImageTag, String hubUser) {
 
     sh """ 
@@ -6,3 +7,14 @@ def call(String project, String ImageTag, String hubUser) {
     """
 
 }
+*/
+
+def call(String aws_account_id, String region, String ecr_repoName) {
+
+    sh """ 
+        trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+        cat scan.txt
+    """
+
+}
+
