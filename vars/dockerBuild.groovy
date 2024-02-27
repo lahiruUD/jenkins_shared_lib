2a7d61e48492e608349e62dc2a543adcc0ce3f5f
lahiruUD <l.udayanga@yahoo.com>
2024-02-27 08:37:04 +0530

    added-4

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,4 @@
+/* 
 def call(String project, String ImageTag, String hubUser) {
 
     sh """ 
@@ -6,4 +7,13 @@ def call(String project, String ImageTag, String hubUser) {
         docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 
+} 
+*/
+
+
+def call(String aws_account_id, String region, String ecr_repoName) {
+    sh """ 
+        docker build -t ${ecr_repoName} .
+        docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
 }
