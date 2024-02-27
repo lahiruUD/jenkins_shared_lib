6e6ebb4c65e4089565cbfbfe3790e3d945a60d2f
lahiruUD <l.udayanga@yahoo.com>
2024-02-27 21:27:54 +0530

    git checkout stage added-5

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -16,8 +16,9 @@ def call(String project, String ImageTag, String hubUser) {
 
 def call(String aws_account_id, String region, String ecr_repoName) {
     sh """ 
-        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-        docker push ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+        docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
+
 
